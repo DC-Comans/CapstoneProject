0c862cbc19dede2c1add69ec17b75a5788ce002d
David <DaveCWorkmail2021@gmail.com>
2025-04-14 00:09:45 +1000

    Login page

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <div class="logo">Healthy End of Life Planning</div>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Take Quiz</a></li>
+        <li><a href="#">Resources</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+</header>
+
+<!-- Top of page -->
+
+{{$slot}}
+
+<!-- Bottom of page -->
+<footer>
+<p>{{date('Y')}}</p>
+<p>All rights reserved</p>
+</footer>
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,40 @@
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="/css/main.css">
+    </head>
+    <body>
+
+        <x-layout>
+            <div class="container">
+                <div class="outer-box">
+                <div class="login-box">
+                  <form>
+                    <label for="username"><strong>Username:</strong></label>
+                    <input type="text" id="username" name="username">
+            
+                    <label for="password"><strong>Password:</strong></label>
+                    <input type="password" id="password" name="password">
+            
+                    <button type="submit" class="btn login-btn">Login</button>
+                  </form>
+                </div>
+            
+                <div class="signup-box">
+                  <span>Don't have an account yet? Just Sign up</span>
+                  <button class="btn signup-btn">Sign Up</button>
+                </div>
+            </div>
+              </div>
+            
+            
+              
+        
+        </body>
+                </html>
+        </x-layout>
+
+
+  
